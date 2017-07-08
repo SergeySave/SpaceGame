0d711a9ddd373d373dce83d6c6d41738ceab8d4a
SergeySave <sergeysav.nn@gmail.com>
2017-07-08 14:18:55 -0400

    makes the amount of the money producer component final

--- core/src/com/sergey/spacegame/common/ecs/component/MoneyProducerComponent.kt
+++ core/src/com/sergey/spacegame/common/ecs/component/MoneyProducerComponent.kt
@@ -6,7 +6,7 @@ import com.badlogic.ashley.core.ComponentMapper
 /**
  * @author sergeys
  */
-class MoneyProducerComponent @JvmOverloads constructor(var amount: Double = 0.0) : ClonableComponent {
+class MoneyProducerComponent @JvmOverloads constructor(val amount: Double = 0.0) : ClonableComponent {
     
     override fun copy(): Component {
         return MoneyProducerComponent(this.amount)
